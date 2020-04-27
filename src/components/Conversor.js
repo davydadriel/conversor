18f3bacd176fe24b4c041be784dde3d43bbb9eff
davydadriel <davyd.adriel@gmail.com>
2020-04-26 23:18:47 -0300

    Criação da pasta para o component e dos arquivos de Conversor.

---
+++ src/components/Conversor.js
@@ -0,0 +1,17 @@
+//import liraries
+import React, { Component } from 'react';
+
+// create a component
+class Conversor extends Component {
+    render() {
+        return (
+            <div className="conversor">
+                <p>Esse é o conversor.</p>
+            </div>
+        );
+    }
+}
+
+
+//make this component available to the app
+export default Conversor;
